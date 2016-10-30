d85bf88ffbc2b4461dd0c1a9a6d5be42c85e5a36
ravindersahni <ravinderssahni@gmail.com>
2016-10-30 16:19:36 -0400

    parse basic input

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	for i, v := range os.Args {
-		if i > 0 {
-			fmt.Printf("%d: %s\n", i, v)
-		}
+	s := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("Who sucks? ")
+	s.Scan()
+	sucker := strings.TrimSpace(s.Text())
+
+	fmt.Printf("What does %s suck? ", sucker)
+	s.Scan()
+	suckables := strings.Split(s.Text(), ",")
+
+	var trimmed []string
+	for _, str := range suckables {
+		trimmed = append(trimmed, strings.TrimSpace(str))
+	}
+
+	for _, suckable := range trimmed {
+		fmt.Printf("%s sucks %s\n", sucker, suckable)
 	}
 }
